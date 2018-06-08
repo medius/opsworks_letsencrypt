43a768a35e947f6c793d6d40ed25859e70fa2e85
Puru Choudhary <puruchoudhary@gmail.com>
2018-06-08 16:15:26 -0700

    Make the standalone port an integer

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,11 +3,10 @@ default[:letsencrypt][:config_dir] = '/etc/letsencrypt'
 default[:letsencrypt][:cert_name] = 'example.com'
 default[:letsencrypt][:webroot_path] = '/var/www/html/'
 default[:letsencrypt][:standalone_host] = '127.0.0.1'
-default[:letsencrypt][:standalone_port] = '80'
+default[:letsencrypt][:standalone_port] = 80
 default[:letsencrypt][:webroot_mode] = true # false => standalone mode
 
 default[:letsencrypt][:ssl_domains] = ['example.com', 'www.example.com']
-
 default[:letsencrypt][:email] = 'abc@example.com' # Letsencrypt account email
 
 default[:letsencrypt][:opsworks_region] = 'us-east-1'
